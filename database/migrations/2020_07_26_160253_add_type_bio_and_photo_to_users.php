61649994be7d7c36e1b788334182c63b8fdfd555
mashfuk <mashfuk@gmail.com>
2020-07-27 05:46:52 +0200

    hello

---
+++ database/migrations/2020_07_26_160253_add_type_bio_and_photo_to_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTypeBioAndPhotoToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('type')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('photo')->nullable();
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ resources/js/components/Users.vue
@@ -0,0 +1,153 @@
+<template>
+    <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Users Table</h3>
+
+                <div class="card-tools">
+
+                    <button class="btn btn-success" data-toggle="modal" data-target="#addNew">Add New <i class="fas fa-user-plus fa-fw"></i></button>
+
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>Id</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Type</th>
+                      <th>Modify</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>183</td>
+                      <td>John Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-success">Approved</span></td>
+                      <td>
+                        <a href="#" @click="editModal(user)">
+                            <i class="fa fa-edit blue"></i>
+                        </a>
+                        /
+                        <a href="#" @click="deleteUser(user.id)">
+                            <i class="fa fa-trash red"></i>
+                        </a>
+
+                    </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addNewLabel">User</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <form @submit.prevent="createUser()">
+
+      <div class="modal-body">
+
+       <div class="form-group">
+       <label>Name</label>
+       <input v-model="form.name" type="text" name="name"
+        class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+       <has-error :form="form" field="name"></has-error>
+       </div>
+
+       <div class="form-group">
+       <label>Email</label>
+       <input v-model="form.email" type="text" name="email"
+        class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+       <has-error :form="form" field="email"></has-error>
+       </div>
+
+       <div class="form-group">
+       <label>Password</label>
+       <input v-model="form.password" type="text" name="password"
+        class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+       <has-error :form="form" field="password"></has-error>
+       </div>
+  
+       <!-- Comment 
+
+        <div class="form-group">
+         <label>Role</label>
+            <select name="type" v-model="form.type" id="type" class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                <option value="admin">Admin</option>
+                <option value="user">Standard User</option>
+                <option value="author">Author</option>
+            </select>
+            <has-error :form="form" field="type"></has-error>
+        </div>
+      
+        -->
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Create</button>
+      </div>
+      
+      </form>
+
+    </div>
+  </div>
+</div>
+
+
+
+</div>
+
+
+
+
+</template>
+
+<script>
+
+import Form from 'vform'; 
+
+    export default {
+        
+         data() {
+            return {
+                form: new Form({
+                    
+                    name : '',
+                    email: '',
+                    password: ''
+                })
+            }
+        },
+        methods: {
+
+         createUser(){
+                
+                this.form.post('api/user');
+                
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
